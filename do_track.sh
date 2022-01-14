fde7b89d541b64d6e64d59fbf1e245b7780f40ec
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-01-14 14:02:06 +0100

    Initial version

---
+++ do_track.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# exit on errors
+set -e
+
+# use 1M nodes for each position
+nodes=1000000
+
+# clone SF as needed
+if [[ ! -e Stockfish ]]; then
+   git clone https://github.com/official-stockfish/Stockfish.git
+fi
+
+# update SF and get revision list
+cd Stockfish/src
+git checkout master >& checkout.log
+git fetch origin  >& fetch.log
+git pull >& pull.log
+revs=`git rev-list dd9cf305816c84c2acfa11cae09a31c4d77cc5a5^..HEAD`
+revs=`git rev-list 7262fd5d14810b7b495b5038e348a448fda1bcc3^..HEAD`
+cd ../..
+
+# go over the revision list and see if we have mate results already
+for rev in $revs
+do
+   file=out.$rev.$nodes
+   if [[ ! -f $file ]]; then
+      echo "generate $file "
+      # compile revision and get binary
+      cd Stockfish/src
+      git checkout $rev >& checkout2.log
+      make clean >& clean.log
+      make -j ARCH=x86-64-modern profile-build >& make.log
+      mv stockfish ../..
+      cd ../..
+
+      # run a matecheck round on this binary
+      python3 matecheck.py --stockfish ./stockfish --nodes $nodes >& out.tmp
+      mv out.tmp $file
+
+      # collect results for this revision
+      mates=`grep Best $file | awk '{print $NF}'`
+      epoch=`git show --pretty=fuller --date=short $rev | grep 'CommitDate' | awk '{print $NF}'`
+      echo "$rev $epoch $mates" >> all_results.txt
+
+      # update the graph
+      awk '{print $2,$3}' all_results.txt | sort  | xmgrace  -param matefinding.xmgrparams - -printfile all_results.svg -hdevice SVG -hardcopy
+   fi
+done
---
+++ matecheck.py
@@ -0,0 +1,80 @@
+import re
+import argparse
+import concurrent.futures
+import chess.engine
+import chess
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
+class Analyser:
+    def __init__(self, engine, nodes):
+        self.engine = engine
+        self.nodes = nodes
+
+    def analyze_fens(self, fens):
+        result_fens = []
+        engine = chess.engine.SimpleEngine.popen_uci(self.engine)
+        for input in fens:
+            board = chess.Board(input[0])
+            info = engine.analyse(board, chess.engine.Limit(nodes=self.nodes))
+            if "score" in info:
+                result_fens.append(
+                    [input[0], input[1], info["score"].pov(board.turn).mate()]
+                )
+            else:
+                result_fens.append([input[0], input[1], None])
+
+        engine.quit()
+
+        return result_fens
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--stockfish", type=str, default="./stockfish", help="Name of the stockfish binary"
+)
+parser.add_argument("--nodes", type=int, default=2000000, help="nodes per pos")
+args = parser.parse_args()
+
+ana = Analyser(args.stockfish, args.nodes)
+
+p = re.compile("([0-8a-zA-Z/\- ]*) bm #([0-9\-]*);")
+fens = []
+
+with open(
+    "ChestUCI_23102018.epd", "r", encoding="utf-8-sig", errors="surrogateescape"
+) as f:
+    for line in f:
+        m = p.match(line)
+        if not m:
+            print("---------------------> IGNORING : ", line)
+        else:
+            fens.append([m.group(1), int(m.group(2))])
+
+fenschunked = list(chunks(fens, 10))
+res = []
+if True:
+    with concurrent.futures.ProcessPoolExecutor() as e:
+        results = e.map(ana.analyze_fens, fenschunked)
+        for r in results:
+            res = res + r
+
+mates = 0
+bestmates = 0
+bettermates = 0
+for r in res:
+    if not r[2]:
+        continue
+    mates = mates + 1
+    if abs(r[1]) == abs(r[2]):
+        bestmates = bestmates + 1
+
+print("Using %s with %d nodes" % (args.stockfish, args.nodes))
+print("Total fens:   ", len(fens))
+print("Found mates:  ", mates)
+print("Best mates:   ", bestmates)
