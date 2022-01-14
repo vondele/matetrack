659e7a86d176cd338afa3e3ae5174ab3fb4cf2d3
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2022-01-14 15:02:22 +0100

    Fix getting date

--- do_track.sh
+++ do_track.sh
@@ -16,8 +16,7 @@ cd Stockfish/src
 git checkout master >& checkout.log
 git fetch origin  >& fetch.log
 git pull >& pull.log
-revs=`git rev-list dd9cf305816c84c2acfa11cae09a31c4d77cc5a5^..HEAD`
-revs=`git rev-list 7262fd5d14810b7b495b5038e348a448fda1bcc3^..HEAD`
+revs=`git rev-list 773dff020968f7a6f590cfd53e8fd89f12e15e36^..HEAD`
 cd ../..
 
 # go over the revision list and see if we have mate results already
@@ -29,6 +28,7 @@ do
       # compile revision and get binary
       cd Stockfish/src
       git checkout $rev >& checkout2.log
+      epoch=`git show --pretty=fuller --date=short $rev | grep 'CommitDate' | awk '{print $NF}'`
       make clean >& clean.log
       make -j ARCH=x86-64-modern profile-build >& make.log
       mv stockfish ../..
@@ -40,10 +40,13 @@ do
 
       # collect results for this revision
       mates=`grep Best $file | awk '{print $NF}'`
-      epoch=`git show --pretty=fuller --date=short $rev | grep 'CommitDate' | awk '{print $NF}'`
       echo "$rev $epoch $mates" >> all_results.txt
 
       # update the graph
       awk '{print $2,$3}' all_results.txt | sort  | xmgrace  -param matefinding.xmgrparams - -printfile all_results.png -hdevice PNG -hardcopy
    fi
 done
+
+git add all_results.txt all_results.png
+git commit -m "Update results"
+git push origin master
