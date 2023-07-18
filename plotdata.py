ac7c65a4d5e907bd940920dd8074ac4f5300205d
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-07-18 14:49:03 +0200

    new recent plot

--- plotdata.py
+++ plotdata.py
@@ -23,27 +23,40 @@ class matedata:
                     self.bmates.append(int(parts[4]))
 
     def create_graph(self, plotAll=False):
+        # plotAll=True: full history, against date, single y-axis
+        # plotAll=False: last 50 commits, against commit, two y-axes
         plotStart = 0 if plotAll else -50
         d, m, b = self.date[plotStart:], self.mates[plotStart:], self.bmates[plotStart:]
-        dotSize, lineWidth = (20, 0.5) if len(d) < 1000 else (5, 0.0)
         fig, ax = plt.subplots()
         yColor, dateColor = "black", "black"
         bmateColor, mateColor = "limegreen", "blue"
-        ax.scatter(d, m, label="mates", color=mateColor, s=dotSize)
-        ax.scatter(d, b, label="best mates", color=bmateColor, s=dotSize)
-        ax.plot(d, m, color=mateColor, linewidth=lineWidth)
-        ax.plot(d, b, color=bmateColor, linewidth=lineWidth)
-        ax.set_ylabel("# of mates", color=yColor)
-        ax.tick_params(axis="y", labelcolor=yColor)
-        ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
-        plt.setp(
-            ax.get_xticklabels(),
-            rotation=45,
-            ha="right",
-            rotation_mode="anchor",
-            fontsize=6,
-        )
-        ax.legend()
+        if plotAll:
+            dotSize = 5
+            ax.scatter(d, m, label="mates", color=mateColor, s=dotSize)
+            ax.scatter(d, b, label="best mates", color=bmateColor, s=dotSize)
+            ax.set_ylabel("# of mates", color=yColor)
+            ax.tick_params(axis="y", labelcolor=yColor)
+            ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+            plt.setp(
+                ax.get_xticklabels(),
+                rotation=45,
+                ha="right",
+                rotation_mode="anchor",
+                fontsize=6,
+            )
+            ax.legend()
+        else:
+            d = list(range(1, -plotStart + 1))
+            ax2 = ax.twinx()
+            dotSize, lineWidth = 20, 0.5
+            ax2.scatter(d, m, label="mates", color=mateColor, s=dotSize)
+            ax.scatter(d, b, label="best mates", color=bmateColor, s=dotSize)
+            ax2.plot(d, m, color=mateColor, linewidth=lineWidth)
+            ax.plot(d, b, color=bmateColor, linewidth=lineWidth)
+            ax.set_ylabel("# of best mates", color=bmateColor)
+            ax.tick_params(axis="y", labelcolor=bmateColor)
+            ax2.set_ylabel("# of mates", color=mateColor)
+            ax2.tick_params(axis="y", labelcolor=mateColor)
         fig.suptitle("Evolution of SF mate finding effectiveness")
         nodes = self.prefix[9:]  #  this will only work for "matetrackXXX"
         if nodes.endswith("0" * 9):
