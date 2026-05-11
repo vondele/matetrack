faf7cf3be95ab7dfdcde6e035de28a30887cd8a5
Robert Nürnberg <28635489+robertnurnberg@users.noreply.github.com>
2026-05-11 12:57:54 +0200

    Apply shfmt (#152)

--- do_track.sh
+++ do_track.sh
@@ -21,49 +21,49 @@ nodes=1000000
 
 # use compact file names for the repo
 if [ "$repo" = "yes" ]; then
-    csv=matetrack$nodes.csv # list of previously computed results
-    new=new$nodes.csv       # temporary list of newly computed results
+  csv=matetrack$nodes.csv # list of previously computed results
+  new=new$nodes.csv       # temporary list of newly computed results
 else
-    csv=matetrack_"$firstrev"_"$lastrev"_"$nodes".csv
-    new=new_"$firstrev"_"$lastrev"_"$nodes".csv
+  csv=matetrack_"$firstrev"_"$lastrev"_"$nodes".csv
+  new=new_"$firstrev"_"$lastrev"_"$nodes".csv
 fi
 out=out.tmp # file for output from matecheck.py
 
 # if necessary, create a new csv file with the correct header
 if [[ ! -f $csv ]]; then
-    echo "Commit Date,Commit SHA,Positions,Mates,Best mates,Better mates,Wrong mates,Bad PVs,Release tag" >$csv
+  echo "Commit Date,Commit SHA,Positions,Mates,Best mates,Better mates,Wrong mates,Bad PVs,Release tag" >$csv
 fi
 
 # check if script is already running, using $new as lock file
 if [[ -f $new ]]; then
-    echo "ERROR: Found '$new', indicating the script is already running."
-    echo "HINT: Merge partial results and/or delete $new before trying again."
-    echo "      The former can be achieved with"
-    echo "      'cat $new >>$csv && rm $new'."
-    echo -e "\nABORTING"
-    exit 1
+  echo "ERROR: Found '$new', indicating the script is already running."
+  echo "HINT: Merge partial results and/or delete $new before trying again."
+  echo "      The former can be achieved with"
+  echo "      'cat $new >>$csv && rm $new'."
+  echo -e "\nABORTING"
+  exit 1
 else
-    touch $new
+  touch $new
 fi
 
 # clone SF (and download an old, non-embedded master net) as needed
 if [[ ! -e Stockfish ]]; then
-    git clone https://github.com/official-stockfish/Stockfish.git
+  git clone https://github.com/official-stockfish/Stockfish.git
 fi
 if [[ ! -f $nnuefile ]]; then
-    wget https://tests.stockfishchess.org/api/nn/$nnuefile
+  wget https://tests.stockfishchess.org/api/nn/$nnuefile
 fi
 
 # update SF, get a sorted revision list and all the release tags
 cd Stockfish/src
 git checkout master >&checkout.log
 if ! git fetch origin >&fetch.log; then
-   echo "Failed fetch!"
-   cat fetch.log
+  echo "Failed fetch!"
+  cat fetch.log
 fi
 if ! git pull >&pull.log; then
-   echo "Failed pull"
-   cat pull.log
+  echo "Failed pull"
+  cat pull.log
 fi
 revs=$(git rev-list --reverse $firstrev^..$lastrev)
 
@@ -78,67 +78,67 @@ cd ../..
 
 # go over the revision list and obtain missing results if necessary
 for rev in $revs; do
-    if ! grep -q "$rev" "$csv"; then
-        cd Stockfish/src
-        git checkout $rev >&checkout2.log
-        epoch=$(git show --pretty=fuller --date=iso-strict $rev | grep 'CommitDate' | awk '{print $NF}')
-        tag=$(echo "$tags" | grep $rev | sed 's/.*\///' | sed 's/sf_5\^{}/sf_5/')
+  if ! grep -q "$rev" "$csv"; then
+    cd Stockfish/src
+    git checkout $rev >&checkout2.log
+    epoch=$(git show --pretty=fuller --date=iso-strict $rev | grep 'CommitDate' | awk '{print $NF}')
+    tag=$(echo "$tags" | grep $rev | sed 's/.*\///' | sed 's/sf_5\^{}/sf_5/')
 
-        # check if revision SHA is in non-comment section of exclude file
-        if ! sed 's/#.*//' "../../$exclude" | grep -q "$rev"; then
-            echo "running matecheck on revision $rev "
+    # check if revision SHA is in non-comment section of exclude file
+    if ! sed 's/#.*//' "../../$exclude" | grep -q "$rev"; then
+      echo "running matecheck on revision $rev "
 
-            # compile revision and get binary
-            make clean >&clean.log
-            arch=x86-64-avx2
-            # for very old revisions, we need to fall back to x86-64-modern
-            if ! grep -q "$arch" Makefile; then
-                arch=x86-64-modern
-            fi
-            CXXFLAGS='-march=native' make -j ARCH=$arch profile-build >&make.log
-            mv stockfish ../..
-            cd ../..
+      # compile revision and get binary
+      make clean >&clean.log
+      arch=x86-64-avx2
+      # for very old revisions, we need to fall back to x86-64-modern
+      if ! grep -q "$arch" Makefile; then
+        arch=x86-64-modern
+      fi
+      CXXFLAGS='-march=native' make -j ARCH=$arch profile-build >&make.log
+      mv stockfish ../..
+      cd ../..
 
-            # run a matecheck round on this binary, being nice to other processes
-            nproc_use=$(nproc)
-            if [ $nproc_use -gt 1 ]; then
-                nproc_use=$((3 * nproc_use / 4))
-            fi
-            nice python3 matecheck.py --engine ./stockfish --nodes $nodes --concurrency $nproc_use >&$out
+      # run a matecheck round on this binary, being nice to other processes
+      nproc_use=$(nproc)
+      if [ $nproc_use -gt 1 ]; then
+        nproc_use=$((3 * nproc_use / 4))
+      fi
+      nice python3 matecheck.py --engine ./stockfish --nodes $nodes --concurrency $nproc_use >&$out
 
-            # collect results for this revision
-            total=$(grep "Total FENs:" $out | awk '{print $3}')
-            mates=$(grep "Found mates:" $out | awk '{print $3}')
-            bmates=$(grep "Best mates:" $out | awk '{print $3}')
-            better=$(grep "Better mates:" $out | awk '{print $3}')
-            wrong=$(grep "Wrong mates:" $out | awk '{print $3}')
-            badpvs=$(grep "Bad PVs:" $out | awk '{print $3}')
+      # collect results for this revision
+      total=$(grep "Total FENs:" $out | awk '{print $3}')
+      mates=$(grep "Found mates:" $out | awk '{print $3}')
+      bmates=$(grep "Best mates:" $out | awk '{print $3}')
+      better=$(grep "Better mates:" $out | awk '{print $3}')
+      wrong=$(grep "Wrong mates:" $out | awk '{print $3}')
+      badpvs=$(grep "Bad PVs:" $out | awk '{print $3}')
 
-            # save wrong/better mates and wrong or incomplete PVs for possible debugging
-            if grep -q issues $out; then
-                mv $out out$nodes.$rev
-            fi
-        else
-            echo "skipping non-viable revision $rev "
-            cd ../..
-            total= mates= bmates= better= wrong= badpvs=
-        fi
-        echo "$epoch,$rev,$total,$mates,$bmates,$better,$wrong,$badpvs,$tag" >>$new
+      # save wrong/better mates and wrong or incomplete PVs for possible debugging
+      if grep -q issues $out; then
+        mv $out out$nodes.$rev
+      fi
+    else
+      echo "skipping non-viable revision $rev "
+      cd ../..
+      total= mates= bmates= better= wrong= badpvs=
     fi
+    echo "$epoch,$rev,$total,$mates,$bmates,$better,$wrong,$badpvs,$tag" >>$new
+  fi
 done
 
 if [ -s $new ]; then
-    cat $new >>$csv
-    rm $new
-    python3 plotdata.py $csv
+  cat $new >>$csv
+  rm $new
+  python3 plotdata.py $csv
 
-    if [ "$repo" = "yes" ]; then
-        git add $csv matetrack$nodes.png matetrack"$nodes"all.png
-        git diff --staged --quiet || git commit -m "Update results"
-        git push origin master >&push.log
-    fi
+  if [ "$repo" = "yes" ]; then
+    git add $csv matetrack$nodes.png matetrack"$nodes"all.png
+    git diff --staged --quiet || git commit -m "Update results"
+    git push origin master >&push.log
+  fi
 else
-    rm -f $new
+  rm -f $new
 fi
 
 echo "ended at: " $(date)
